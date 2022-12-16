17cf75c0117f7dd23a08398974715bbcf695e843
Lusindiso Ntanjana <lusindisontanjana@gmail.com>
2022-12-16 03:29:50 +0200

    Create owner's and species tables.

--- schema.sql
+++ schema.sql
@@ -8,8 +8,34 @@ CREATE TABLE animals
   date_of_birth date,
   escape_attempts integer,
   neutered boolean,
-  weight_kg real
+  weight_kg decimal
 );
 
 ALTER TABLE animals  
 ADD COLUMN species varchar(100);
+
+
+CREATE TABLE owners
+(
+	id serial primary key,
+	full_name varchar(50),
+	age integer
+);
+
+CREATE TABLE species
+(
+	id serial primary key,
+	name varchar(100)
+);
+
+ALTER TABLE animals 
+ADD COLUMN owner_id integer;
+
+ALTER TABLE animals 
+ADD COLUMN species_id integer;
+
+ALTER TABLE animals 
+ADD CONSTRAINT animalspecies FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals 
+ADD CONSTRAINT animalsowner FOREIGN KEY (owner_id) REFERENCES owners(id);
